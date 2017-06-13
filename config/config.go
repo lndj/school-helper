dbc5875edc1295ec7bab0748f2fc79eb8b0642d0
Ning Luo <lnanddj@gmail.com>
2017-06-14 00:31:11 +0800

    完善配置获取

--- config/config.go
+++ config/config.go
@@ -2,24 +2,47 @@ package config
 
 import (
 	"github.com/labstack/gommon/log"
-	configer "github.com/olebedev/config"
+	"github.com/murlokswarm/errors"
+	cg "github.com/olebedev/config"
+	"io/ioutil"
+	"os"
 )
 
-func Get(env string) interface{} {
-	cfg, err := configer.ParseYaml("./config.yaml")
+type Config struct {
+	Content *cg.Config
+}
+
+func NewConfig() (*cg.Config, error) {
+	path, _ := os.Getwd()
+	cfgFile, err := ioutil.ReadFile(path + "/config/config.yaml")
+	if err != nil {
+		log.Fatalf("Read yaml file error:%v", err)
+		return nil, errors.New("Read yaml file error:%v", err)
+	}
+
+	cfg, err := cg.ParseYaml(string(cfgFile))
 
 	if err != nil {
 		log.Fatalf("Parse yaml file error:%v", err)
+		return nil, errors.New("Parse yaml file error:%v", err)
+	}
+	cfg.Env()
 
-		return nil
+	//根据当前环境变量获取配置
+	env, err := cfg.String("ENV")
+
+	if err != nil {
+		log.Fatalf("Get env error:%v", err)
+		return nil, errors.New("Get ENV error:%v", err)
 	}
 
-	content, err := cfg.Get(env)
+	ct := new(Config)
+	ct.Content, err = cfg.Get(env)
 
 	if err != nil {
 		log.Fatalf("Get config error:%v", err)
-		return nil
+		return nil, errors.New("Get config error:%v", err)
 	}
 
-	return content
+	return ct.Content, nil
 }
